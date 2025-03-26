139d547684c82dc73051327494f4040779fdb9ca
kamlesh gangaramani <kemails2006@gmail.com>
2025-03-26 17:57:10 +0530

    Add basic GitHub Actions workflow for Flask CI

--- app.py
+++ app.py
@@ -253,4 +253,4 @@ def rectify(commit_hash):
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     print(f"Service running on port {port}")
-    app.run(host='0.0.0.0', port=port)
+    app.run(port=port)
